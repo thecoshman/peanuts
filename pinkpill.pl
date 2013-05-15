f65c00b0ddb6bbd1edcb9061d907f1406bf02fa8
thecoshman <thecoshman@gmail.com>
2013-05-15 20:31:17 +0100

    And pow! GLLoad is in the house. Build system is also now able to compile most files.

--- pinkpill.pl
+++ pinkpill.pl
@@ -3,10 +3,11 @@ use strict;
 use warnings;
 use PinkPill;
 
+my $common_folders = 'src !^src/platform src/platform/common nix^src/platform/linux win^src/platform/windows osx^src/platform/osx '; 
 my $pp = new PinkPill;
 $pp->set_options(
-    src_folders => 'src !^src/platform src/platform/common nix^src/platform/linux win^src/platform/windows osx^src/platform/osx',
-    inc_folders => 'src !^src/platform src/platform/common nix^src/platform/linux win^src/platform/windows osx^src/platform/osx dependencies/boost_1_53_0',
+    src_folders => $common_folders,
+    inc_folders => $common_folders . 'dependencies/boost_1_53_0 dependencies/glsdk_0.4.4/glload/include',
     build_folder => 'bin',
     obj_folder => 'bin/obj',
     compiler_flags => '-std=c++11 -Wall -Wextra -DPEANUTS_LINUX',
--- src/platform/linux/main.cpp
+++ src/platform/linux/main.cpp
@@ -2,6 +2,5 @@
 #include "windowImplementation.hpp"
 
 int main() {
-//    Window window(Kyro::Platform::Windowed(std::pair<int, int>(100, 100), std::pair<int, int>(10, 10));
-    return Kyro::Main();
+    return Peanuts::Main();
 }
